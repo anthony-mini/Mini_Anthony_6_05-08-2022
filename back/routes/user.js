d9394ee4ce7ac1e9b9978d6de94ce411f68f4974
anthony-mini <anthony-mini@outlook.fr>
2022-10-28 11:15:32 +0200

    Modification du ratelimite pour la création de compte

--- back/routes/user.js
+++ back/routes/user.js
@@ -15,8 +15,8 @@ const rateLimit = require('express-rate-limit')
 const userCtrl = require('../controllers/user');
 
 const createAccountLimiter = rateLimit({
-	windowMs : 10 * 60 * 1000, // 10 minutes
-	max : 2, // Limitez chaque IP à 2 création des demandes de compte par `fenêtre` (ici, par tranche de 10 minutes)
+	windowMs : 30 * 60 * 1000, // 30 minutes
+	max : 5, // Limitez chaque IP à 5 création des demandes de compte par `fenêtre` (ici, par tranche de 30 minutes)
 	message : 'Trop de comptes créés à partir de cette IP, veuillez réessayer dans 30 minutes',
 	standardHeaders : true, // Informations sur la limite de taux de retour dans les en-têtes `RateLimit-*`
 	legacyHeaders : false, // Désactiver les en-têtes `X-RateLimit-*`
