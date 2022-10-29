02eb01fcb89c2d2018f44dd518589bc83e243912
anthony-mini <anthony-mini@outlook.fr>
2022-10-29 18:40:03 +0200

    Edit des commentaires

--- back/app.js
+++ back/app.js
@@ -18,9 +18,13 @@ const rateLimit = require('express-rate-limit')
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
+// Appel de l'application express.
+const app = express();
+
 /**
- * https://www.npmjs.com/package/express-rate-limit
- */
+* ** Fonction limitant le nombre de requête pour un utilisateur avec le package : Express-rate-limit **
+* https://www.npmjs.com/package/express-rate-limit
+*/
 
 const limiter = rateLimit({
 	windowMs: 10 * 60 * 1000, // 10 minutes
@@ -30,52 +34,62 @@ const limiter = rateLimit({
 });
 
 /**
- * ** Connexion à la base de données MongoDB via Mongoose **
- */
+* ** Connexion à MongoDB via Mongoose **
+*/
 
 mongoose.connect(
 `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_NAME}/?retryWrites=true&w=majority`,
-{ useNewUrlParser: true, useUnifiedTopology: true }
-)
+{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-  
-  // Appel de l'application express.
-  const app = express();
+ 
 
-  /**
-  * Analyser du corps de la requête au format JSON,
-  * venant de l'application front-end.
-  */
+/** 
+* Analyser du corps de la requête au format JSON, venant de l'application front-end.
+*/
     
 app.use(express.json());
+
+/**
+* Sécurisation de l'application Express avec le package : Helmet
+* https://www.npmjs.com/package/helmet
+*/
 
 app.use(
   helmet({
     crossOriginResourcePolicy: false, // Désactive la sécurité lors de la récupération des images.
-    contentSecurityPolicy : false,
-    //referrerPolicy: { policy: "unsafe-url" }, // Désactive la sécurité lors de l'intération avec la modification des sauces.
+    contentSecurityPolicy : false, // Désactive la sécurité lors de l'intération avec la modification des sauces.
   })
 );
  
+// Appel de la fonction Limiter.
 app.use(limiter);
  
- /**
-  * ** Création d'un header : **
+/**
+  * ------ CORS ------
+  * ** Création d'un header **
   * #. Débloque les erreus CORS
-  * #. Autorise l'acces à notre API
-  */
+  * #. Autorise l'acces à notre API :
+  *     . Configure quels headers sont authorisés
+  *     . Configure quels méthodes sont authorisés
+*/
  
- app.use((req, res, next) => {
-   res.setHeader('Access-Control-Allow-Origin', '*'); // Acces à l'API selon n'importe quel origine ('*')
-   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // ajout des headers mentionnés aux requêtes envoyées vers notre API
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // envoie des requêtes avec les méthodes mentionnées ( GET ,POST , etc.)
-   next();
-  });
+app.use((req, res, next) => {
+
+    // Acces à l'API selon n'importe quel origine ('*')
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    // Ajout des headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
+    // Envoie des requêtes avec les méthodes mentionnées ( GET ,POST , etc.)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
   
-
-
-// Importation du segment d'origine des routes 
+    next();
+});
+  
+/**
+ * ----- ROUTING -----
+ * Importation des segments d'origine des routes 
+ */ 
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', sauceRoutes);
